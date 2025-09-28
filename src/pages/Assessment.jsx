b903aa5f6a8faf045b15ab129b569bd50caf23e3
Shravan Padale <spadale771@gmail.com>
2025-09-29 01:38:20 +0530

    finally

--- src/pages/Assessment.jsx
+++ src/pages/Assessment.jsx
@@ -213,7 +213,7 @@ const Assessment = () => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
 
         try {
-            const response = await fetch("/api/predict", {
+            const response = await fetch("https://diapredict-backend-l8d9.onrender.com/predict", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData)
